91ac5ed516cc17eca14aeac0017b396196a99166
Vladick <vlad2004ki@gmail.com>
2025-11-12 16:57:15 +0300

    auto-migrations complete

--- app/config.py
+++ app/config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Settings:
-    DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql://admin:admin@localhost:5432/reshaka")
+    DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql://admin:admin@database:5432/reshaka")
 
 settings = Settings()
